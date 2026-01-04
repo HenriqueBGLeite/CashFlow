81fb1e384d51925dc5e47470661d6148a5e6e233
HenriqueBGLeite <henriquecebrac37@gmail.com>
2026-01-04 20:21:16 -0300

    feat: finalizando busca por id e tratando exceções

--- src/CashFlow.Application/UseCases/Expenses/GetById/GetExpenseByIdUseCase.cs
+++ src/CashFlow.Application/UseCases/Expenses/GetById/GetExpenseByIdUseCase.cs
@@ -1,5 +1,7 @@
 ﻿using CashFlow.Communication.Responses;
 using CashFlow.Domain.Repositories.Expenses;
+using CashFlow.Exception;
+using CashFlow.Exception.ExceptionsBase;
 using Mapster;
 
 namespace CashFlow.Application.UseCases.Expenses.GetById;
@@ -16,6 +18,8 @@ public class GetExpenseByIdUseCase : IGetExpenseByIdUseCase
     public async Task<ResponseExpenseJson> Execute(long id)
     {
         var result = await _repository.GetById(id);
+        if (result is null)
+            throw new NotFoundException(ResourceErrorMessages.EXPENSE_NOT_FOUND);
 
         return result.Adapt<ResponseExpenseJson>();
     }
