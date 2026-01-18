5ff3adb98df5c359729a9963ffb0bfdd43188e09
HenriqueBGLeite <henriquecebrac37@gmail.com>
2026-01-18 19:14:34 -0300

    feat: finaliza testes de unidade e integração para os reports

---
+++ tests/UseCases.Test/Expenses/Reports/Excel/GenerateExpensesReportExcelUseCaseTest.cs
@@ -0,0 +1,46 @@
+﻿using CashFlow.Application.UseCases.Expenses.Reports.Excel;
+using CashFlow.Domain.Entities;
+using CashFlow.Domain.Enums;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.LoggedUser;
+using CommonTestUtilities.Repositories;
+using Shouldly;
+
+namespace UseCases.Test.Expenses.Reports.Excel;
+
+public class GenerateExpensesReportExcelUseCaseTest
+{
+    [Fact]
+    public async Task Success()
+    {
+        var loggedUser = UserBuilder.Build(Roles.ADMIN);
+        var expenses = ExpenseBuilder.Collection(loggedUser);
+
+        var useCase = CreateUseCase(loggedUser, expenses);
+
+        var result = await useCase.Execute(DateOnly.FromDateTime(DateTime.Today));
+
+        result.ShouldNotBeNull();
+        result.ShouldNotBeEmpty();
+    }
+
+    [Fact]
+    public async Task Success_Empty()
+    {
+        var loggedUser = UserBuilder.Build(Roles.ADMIN);
+
+        var useCase = CreateUseCase(loggedUser, []);
+
+        var result = await useCase.Execute(DateOnly.FromDateTime(DateTime.Today));
+
+        result.ShouldBeEmpty();
+    }
+
+    private GenerateExpensesReportExcelUseCase CreateUseCase(User user, List<Expense> expenses)
+    {
+        var repository = new ExpensesReadOnlyRepositoryBuilder().FilterByMonth(user, expenses).Build();
+        var loggedUser = LoggedUserBuilder.Build(user);
+
+        return new GenerateExpensesReportExcelUseCase(repository, loggedUser);
+    }
+}
