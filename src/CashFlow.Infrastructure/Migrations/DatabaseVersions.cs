0c5aa7113101e52d290681fba4f90994faa28366
HenriqueBGLeite <henriquecebrac37@gmail.com>
2026-01-24 13:14:55 -0300

    feat: adicionando Tags

--- src/CashFlow.Infrastructure/Migrations/DatabaseVersions.cs
+++ src/CashFlow.Infrastructure/Migrations/DatabaseVersions.cs
@@ -4,4 +4,5 @@ internal sealed class DatabaseVersions
 {
     internal const long INITIAL_MIGRATION = 1;
     internal const long ALTER_COLUMNS_DESCRIPTION_AND_AMOUNT_IN_EXPENSE = 2;
+    internal const long ADD_NEW_TABLE_TAG = 3;
 }
---
+++ src/CashFlow.Infrastructure/Migrations/Versions/Version00000003.cs
@@ -0,0 +1,18 @@
+﻿using FluentMigrator;
+using System.Data;
+
+namespace CashFlow.Infrastructure.Migrations.Versions;
+
+[Migration(DatabaseVersions.ADD_NEW_TABLE_TAG, "Add new table Tag")]
+public class Version00000003 : ForwardOnlyMigration
+{
+    public override void Up()
+    {
+        Create.Table("Tags")
+            .WithColumn("Id").AsInt64().NotNullable()
+                .PrimaryKey().Identity()
+            .WithColumn("Value").AsInt16().NotNullable()
+            .WithColumn("ExpenseId").AsInt64().NotNullable()
+                .ForeignKey("FK_Tags_ExpenseId", "Expenses", "Id").OnDelete(Rule.Cascade);
+    }
+}
